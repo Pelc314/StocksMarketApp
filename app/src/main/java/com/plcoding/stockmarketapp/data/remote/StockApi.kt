5485978c0a16ecba2c9b947233691c73e1301bf4
Maciej Pelc <78445032+Pelc314@users.noreply.github.com>
2023-01-12 16:28:19 +0100

    created company info data transfer object and intraday data transfer object which are going to be used to display data on the company info screen.

--- app/src/main/java/com/plcoding/stockmarketapp/data/remote/StockApi.kt
+++ app/src/main/java/com/plcoding/stockmarketapp/data/remote/StockApi.kt
@@ -1,5 +1,6 @@
 package com.plcoding.stockmarketapp.data.remote
 
+import com.plcoding.stockmarketapp.data.remote.dto.CompanyInfoDto
 import okhttp3.ResponseBody
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -8,6 +9,18 @@ interface StockApi {
     @GET("query?function=LISTING_STATUS")
     suspend fun getListings(
         @Query("apikey") apiKey: String = API_KEY
+    ): ResponseBody
+
+    @GET("query?function=OVERVIEW")
+    suspend fun getCompanyInfo(
+        @Query("apikey") apiKey: String = API_KEY,
+        @Query("symbol") symbol: String
+    ): CompanyInfoDto
+
+    @GET("query?function=TIME_SERIES_INTRADAY&interval=60min&datatype=csv")
+    suspend fun getIntradayInfo(
+        @Query("apikey") apiKey: String = API_KEY,
+        @Query("symbol") symbol: String
     ): ResponseBody
 
     companion object {
